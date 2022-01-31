ed2dde536e60c6314d3bc32af4a35b985c52534f
Iago Barbosa <45849955+skyrippa@users.noreply.github.com>
2022-01-31 10:19:29 -0300

    :sparkles: Google 2FA middleware

---
+++ src/Http/Middlewares/Google2fa.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use PragmaRX\Google2FA\Google2FA as TwoFactor;
+use Illuminate\Support\Facades\Auth;
+
+class Google2fa
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(app()->runningUnitTests()){
+            return $next($request);
+        }
+        $user = Auth::user();
+        if ($user->two_factor_enable) {
+            if(isset($request->secret)){
+                if((new TwoFactor ())->verifyKey($user->secret_key, $request->secret)){
+                    return $next($request);
+                }
+                else{
+                    return response()->json(['message' =>'Código Inválido'], 422);
+                }
+            }
+            else{
+                return response()->json(['message' =>'Campo secret is missing'], 422);
+            }
+        }
+        else{
+            return response()->json(['message' =>'Autenticação não está habilitada.'], 422);
+        }
+    }
+}
