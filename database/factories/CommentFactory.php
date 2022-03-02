83919c3b5bccb762ebe5b0e7826b95d119f3f868
Iago Barbosa <45849955+skyrippa@users.noreply.github.com>
2022-03-02 12:47:46 -0300

    :recycle:

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,7 +2,6 @@
 
 namespace Lara\Jarvis\Database\Factories;
 
-use Lara\Jarvis\Models\City;
 use Lara\Jarvis\Models\Comment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Lara\Jarvis\Tests\User;
@@ -23,13 +22,13 @@ class CommentFactory extends Factory
      */
     public function definition()
     {
-        $commentable = City::factory()->create();
-        $author = User::factory()->create();
+        $commentable = User::factory()->create();
+        $author      = User::factory()->create();
 
         return [
-            'text' => $this->faker->sentence,
-            'user_id' => $author->id,
-            'commentable_id' => $commentable->id,
+            'text'             => $this->faker->sentence,
+            'user_id'          => $author->id,
+            'commentable_id'   => $commentable->id,
             'commentable_type' => get_class($commentable)
         ];
     }
