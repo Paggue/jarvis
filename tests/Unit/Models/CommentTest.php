de1de190fb14ffdba728f0225e08bef3138e61d0
Iago Barbosa <45849955+skyrippa@users.noreply.github.com>
2022-03-02 12:59:51 -0300

    :white_check_mark: comment

--- tests/Unit/Models/CommentTest.php
+++ tests/Unit/Models/CommentTest.php
@@ -6,17 +6,20 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Lara\Jarvis\Models\City;
 use Lara\Jarvis\Models\Comment;
 use Lara\Jarvis\Tests\TestCase;
+use Lara\Jarvis\Tests\User;
 
 class CommentTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    function a_comment_has_a_text ()
+    function a_comment_has_a_text()
     {
         $model = Comment::factory()->create([
             'text'             => 'Fake Title',
-            'commentable_type' => City::class
+            'commentable_type' => City::class,
+            'commentable_id'   => City::factory()->create()->id,
+            'user_id'          => User::factory()->create()->id,
         ]);
 
         $this->assertEquals('Fake Title', $model->text);
